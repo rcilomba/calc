fd054a33799fee2e89b064c0f29a98d5ad1460cd
rcilomba <rcilomba@yahoo.com>
2025-01-16 01:53:00 +0100

    buttons number, operators, display and clear

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let firstNumber;
+let secondNumber;
+let operate;
+
 function add(firstNumber, secondNumber){
     return firstNumber + secondNumber;
 
 }
 
-function substract(firstNumber, secondNumber){
+function subtract(firstNumber, secondNumber){
     return firstNumber - secondNumber;
     
 }
@@ -16,4 +20,19 @@ function multiply(firstNumber, secondNumber){
 function divide(firstNumber, secondNumber){
     return firstNumber / secondNumber;
     
+}
+
+function operate(firstNumber, operator, secondNumber){
+    if(operator === "+"){
+        return add(firstNumber, secondNumber)
+    } else if (operator === "-"){
+        return subtract(firstNumber, secondNumber)
+    } else if (operator === "*"){
+        return multiply(firstNumber, secondNumber)
+    } else if(operator === "/"){
+        return divide(firstNumber, secondNumber)
+    } else {
+    return alert("add correct operator")
+    }
+
 }
